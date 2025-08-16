d2eaa074232bac2559fa08ea88b3469c3205a7c8
withnoclout <wpong.debug@gmail.com>
2025-08-16 06:08:16 +0000

    update:routine

---
+++ quiz_smallestKTH.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        n = len(matrix)
+        
+        def count_less_equal(mid):
+            count = 0
+            row, col = n - 1, 0
+            while row >= 0 and col < n:
+                if matrix[row][col] <= mid:
+                    count += row + 1
+                    col += 1
+                else:
+                    row -= 1
+            return count
+        
+        left, right = matrix[0][0], matrix[-1][-1]
+        
+        while left < right:
+            mid = (left + right) // 2
+            if count_less_equal(mid) < k:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return left
