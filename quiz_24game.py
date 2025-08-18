ba55b1834da0d000aecff62256fe00c85eda099c
withnoclout <wpong.debug@gmail.com>
2025-08-18 07:32:41 +0000

    update routine

---
+++ quiz_24game.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def judgePoint24(self, cards):
+        """
+        :type cards: List[int]
+        :rtype: bool
+        """
+        EPS  = 1e-6 
+        def dfs(nums ):
+            if len(nums) == 1:
+                return abs(nums[0] - 24.0) < EPS
+
+            for i in range(len(nums)):
+                for j in range(len(nums)):
+                    if i == j:
+                        continue
+                    next_nums = [nums[k] for k in range(len(nums)) if k != i and k != j]
+                    a, b = nums[i], nums[j]
+                    candidates = [a + b, a - b, b - a, a * b]
+                    if abs(b) > EPS:
+                        candidates.append(a / b)
+                    if abs(a) > EPS:
+                        candidates.append(b / a)
+
+                    for val in candidates:
+                        if dfs(next_nums + [val]):
+                            return True
+            return False
+
+        return dfs([float(x) for x in cards])
