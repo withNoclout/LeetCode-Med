0ec4e8ce1bc4c2c5fc391ec030a8bb8fcccdfc2c
withnoclout <wpong.debug@gmail.com>
2025-09-07 01:32:51 +0000

    update Routine

---
+++ quiz_mountainarray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        l , r =  0 , len(arr) -  1   
+        while l < r : 
+            m = ( l + r ) // 2   
+            if arr[m] < arr[m+1]  : 
+                l = m + 1
+            else:
+                r = m
+        return l    
