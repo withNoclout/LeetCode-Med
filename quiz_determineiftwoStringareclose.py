c50bb6c6a4ae611990f4b030ac306e546bc646fd
withnoclout <wpong.debug@gmail.com>
2025-10-13 05:18:18 +0000

    updateRoutine

---
+++ quiz_determineiftwoStringareclose.py
@@ -0,0 +1,16 @@
+
+from collections import Counter
+
+class Solution(object):
+    def closeStrings(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: bool
+        """
+        if len(word1) != len(word2):
+            return False
+        c1, c2 = Counter(word1), Counter(word2)
+        if set(c1.keys()) != set(c2.keys()):
+            return False
+        return sorted(c1.values()) == sorted(c2.values())
